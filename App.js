c68f70da7303fb595eb671cdebfb7af9a96aea21
Kashish Grover <kashish.grover@treebohotels.com>
2018-06-30 12:16:43 +0530

    cleanup for boilerplate code

--- App.js
+++ App.js
@@ -1,46 +1,24 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView, Text } from 'react-native';
 import MyStatusBar from './components/MyStatusBar';
-import RootNavigation from './navigation/RootNavigation';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
+    backgroundColor: '#eee',
   },
 });
 
-const showApiCalls = () => {
-  const baseUrl = 'http://www.mocky.io/';
-  global._fetch = fetch;
-  global.fetch = async (uri, options, ...args) => {
-    const response = await global._fetch(uri, options, ...args);
-    if (uri.includes(baseUrl)) {
-      console.log(
-        '🔵 API Call: ',
-        uri,
-        { request: { uri }, response },
-      );
-    }
-    return response;
-  };
-};
-
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    if (__DEV__) {
-      console.disableYellowBox = true;
-      showApiCalls();
-    }
-  }
+  state = {};
 
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <MyStatusBar />
-        <RootNavigation />
+        <Text>
+          This boilerplate is noice.
+        </Text>
       </SafeAreaView>
     );
   }
